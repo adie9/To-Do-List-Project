f3e3530c9c9cfd06b2a335efa63e0c3d6ff23041
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2023-02-17 16:44:45 -0500

    First Commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function inputToList() {
+    let ul = document.getElementById('tasks') // List variable
+    let input_Value = document.getElementById("new-task").value; // input value
+    
+    let new_li = document.createElement('li'); // Create new list item
+    new_li.textContent = input_Value; // Insert input value into new li
+    ul.appendChild(new_li); // Add new li to list
+    input
+
+    // Create "delete" button
+    
+
+    // Create "completed" button
+
+}
